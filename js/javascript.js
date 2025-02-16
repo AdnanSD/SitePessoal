af74f7354c447289b964e96eab3fab3b471a33e3
AdnanSD <79409063+AdnanSD@users.noreply.github.com>
2025-02-16 19:58:41 -0300

    modo dark-icone

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,18 @@
-const $html=document.querySelector('html');
-const $checkbox=document.querySelector('#switch');
-
-$checkbox.addEventListener('change', function (){
-    $html.classList.toggle('dark-mode')
-});
 
 
 
 function clicar (){
 const $html=document.querySelector('html');
 $html.classList.toggle('dark-mode')
+
+let img=document.getElementById("icone")
+if(img.src.includes("icons8-noite-com-estrelas-48.png")){
+    img.src="/imagens/sol.png";
+}else{
+    img.src="/imagens/icons8-noite-com-estrelas-48.png";
+}
+
+
+
 
 }
