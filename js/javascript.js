fffbebb32a5e651e8d03d7d0d6232d290e58cb4e
AdnanSD <79409063+AdnanSD@users.noreply.github.com>
2025-02-15 21:21:55 -0300

    adcionado dark-mod

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+const $html=document.querySelector('html');
+const $checkbox=document.querySelector('#switch');
+
+$checkbox.addEventListener('change', function (){
+    $html.classList.toggle('dark-mode')
+});
+
+
+
+function clicar (){
+const $html=document.querySelector('html');
+$html.classList.toggle('dark-mode')
+
+}
