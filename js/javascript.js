e92be8fe0a020782d5d0771a540026f5eac3f033
AdnanSD <79409063+AdnanSD@users.noreply.github.com>
2025-02-17 21:25:47 -0300

    att

--- js/javascript.js
+++ js/javascript.js
@@ -7,9 +7,9 @@ $html.classList.toggle('dark-mode')
 
 let img=document.getElementById("icone")
 if(img.src.includes("icons8-noite-com-estrelas-48.png")){
-    img.src="/imagens/sol.png";
+    img.src="imagens/sol.png";
 }else{
-    img.src="/imagens/icons8-noite-com-estrelas-48.png";
+    img.src="imagens/icons8-noite-com-estrelas-48.png";
 }
 
 }
